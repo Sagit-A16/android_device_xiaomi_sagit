e15a51ea4c970bdd4803daf1538ed00b5dd6c434
deny2311 <deny2311@gmail.com>
2026-02-04 06:23:05 +0000

    sagit: Clone RKSU and hardware_xiaomi

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+rm -rf hardware/xiaomi
+git clone -b lineage-23.0 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+rm -rf kernel/xiaomi/msm8998
+git clone --recurse-submodules -b bqx https://github.com/Sagit-A16/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
+rm -rf system/extras/su
