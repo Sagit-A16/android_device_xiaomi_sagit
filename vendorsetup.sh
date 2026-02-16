9c72ffcfd1c4a47cadb85ad0d0c6e7f29f73ac30
denysaja <143594138+denysaja@users.noreply.github.com>
2026-02-16 21:47:07 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf hardware/xiaomi
 git clone -b lineage-23.0 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 rm -rf kernel/xiaomi/msm8998
-git clone --recurse-submodules -b bq2 https://github.com/Sagit-A16/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
+git clone --recurse-submodules -b susfs https://github.com/Sagit-A16/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 rm -rf system/extras/su
