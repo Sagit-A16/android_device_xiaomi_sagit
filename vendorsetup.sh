88622bf355ca1855673a903679727ebaf0e0f1fc
deny <77781846+deny2311@users.noreply.github.com>
2026-04-16 16:20:55 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf hardware/xiaomi
-git clone -b lineage-23.0 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-23.2 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 rm -rf kernel/xiaomi/msm8998
 git clone -b bq2-rksu https://github.com/Sagit-A16/android_kernel_xiaomi_msm8998-cip.git kernel/xiaomi/msm8998
 cd kernel/xiaomi/msm8998
